e588a772f7c837610d61b786a2e29b8c88770d13
Ahmad Abdullah Hashmi <134776723+AhmadAbdullah001@users.noreply.github.com>
2025-01-12 00:50:35 +0530

    Delete backend directory

--- backend/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose')
-const mongoURI = "mongodb+srv://ahmadhashmi1304:xt2kut17@cluster0.oe36p.mongodb.net/";
-async function connectToMongo() {
-    await mongoose.connect(mongoURI).then(()=>{console.log("connected Successfully")}).catch((err)=>{console.log(err)})
-}
-module.exports=connectToMongo;
